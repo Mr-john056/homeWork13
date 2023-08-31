9989abd8766a9f0ba9156f2d95c36e293ea906f9
Mr-john056 <ekulakov056@gmail.com>
2023-08-31 07:02:23 +0500

    Исправил equals

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String surname;
@@ -23,5 +25,18 @@ public class Author {
                 "Имя автора: '" + name + '\'' +
                 ", Фамилия автора: '" + surname + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
     }
 }
